ab1b214c7e88e75f0d5834e61f6c2ccfb45a37ab
Adriana Trujillo <a.trujilloa1@uniandes.edu.co>
2018-10-24 09:12:02 -0500

    Listo listar factura y Tarjeta de credito

--- src/app/factura/factura.module.ts
+++ src/app/factura/factura.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FacturaListComponent } from './factura-list/factura-list.component';
+import {FacturaService} from './factura.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [FacturaListComponent],
+  providers: [FacturaService],
   exports: [FacturaListComponent]
   
 })
---
+++ src/app/factura/factura.ts
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ export class Factura{
+     /**
+      * El numero de la factura
+      */
+      numero: number;
+      
+      /**
+       * La fecha de la factura
+       */
+       date: string;
+       
+       /**
+        * Valor de la factura
+        */
+        valor: number;
+        
+        /**
+         * Metodo de pago de la factura
+         */
+         pago: string;
+ }
+
+
