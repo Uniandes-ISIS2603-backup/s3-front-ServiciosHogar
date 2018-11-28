22599997c95b3482e4923421fca47c196db06297
María Ocampo <mj.ocampov@uniandes.edu.co>
2018-11-28 16:04:51 -0500

    Correccion

--- src/app/prestador/prestador-detail/prestador-detail.component.ts
+++ src/app/prestador/prestador-detail/prestador-detail.component.ts
@@ -1,18 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-<<<<<<< HEAD
-import {PrestadorService} from '../prestador.service';
-import {Prestador} from '../prestador';
-import {PrestadorDetail} from '../prestador-detail';
-import {PrestadorHojaDeVidaComponent} from '../prestador-hojaDeVida/prestador-hojaDeVida.component';
-import {PrestadorAddHojaDeVidaComponent} from '../prestador-add-hojaDeVida/prestador-add-hojaDeVida.component';
 import { Servicio } from 'src/app/servicio/servicio';
-=======
 import { PrestadorService } from '../prestador.service';
 import { Prestador } from '../prestador';
 import { PrestadorDetail } from '../prestador-detail';
->>>>>>> origin/Ciclo3
 
 @Component({
     selector: 'app-prestador-detail',
@@ -33,19 +25,11 @@ export class PrestadorDetailComponent implements OnInit {
     ) { }
 
     /**
-    * The prestador whose details we want to show
-    */
-    prestadorDetail: PrestadorDetail;
-
-
-
-    /**
     * The prestador's id retrieved from the address
     */
     prestador_id: number;
 
     /**
-<<<<<<< HEAD
     * The prestador whose details are shown
     */
     prestadorDetail: PrestadorDetail;
@@ -66,48 +50,16 @@ export class PrestadorDetailComponent implements OnInit {
     */
     navigationSubscription;
 
-
     /**
-     * The child PrestadorHojaDeVidaListComponent
-     */
-    @ViewChild(PrestadorHojaDeVidaComponent) hojaDeVidaListComponent: PrestadorHojaDeVidaComponent;
-
-    /**
-     * The child PrestadorHojaDeVidaListComponent
-     */
-    @ViewChild(PrestadorAddHojaDeVidaComponent) hojaDeVidaAddComponent: PrestadorAddHojaDeVidaComponent;
-
-    toggleHojaDeVida(): void {
-        if (this.hojaDeVidaAddComponent.isCollapsed == false) {
-            this.hojaDeVidaAddComponent.isCollapsed = true;
-        }
-        this.hojaDeVidaListComponent.isCollapsed = !this.hojaDeVidaListComponent.isCollapsed;
-    }
-
-    toggleCreateHojaDeVida(): void {
-        if (this.hojaDeVidaListComponent.isCollapsed == false) {
-            this.hojaDeVidaListComponent.isCollapsed = true;
-        }
-        this.hojaDeVidaAddComponent.isCollapsed = !this.hojaDeVidaAddComponent.isCollapsed;
-    }
-
-
-    /**
-    * The method which retrieves the details of the prestador that
-    * we want to show
-=======
+    * The method which re
     * The method which retrieves the  of an prestador
->>>>>>> origin/Ciclo3
     */
     getPrestadorDetail(): void {
         this.prestadorService.getPrestadorDetail(this.prestador_id)
             .subscribe(prestadorDetail => {
-<<<<<<< HEAD
                 this.prestadorDetail = prestadorDetail;
                 this.servicios = prestadorDetail.servicios;
-=======
                 this.prestadorDetail = prestadorDetail
->>>>>>> origin/Ciclo3
             });
     }
 
